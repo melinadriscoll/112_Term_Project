87190099aef66d321d8e600ebc26748ee10dd2a0
melinadriscoll <melina.driscoll@gmail.com>
2018-04-26 01:51:31 -0400

    Add files via upload

--- pacman.py
+++ pacman.py
@@ -11,10 +11,18 @@ def init(data):
     data.loseState = False
     data.winState = False
     data.margin = 10
+    data.startCircles = [[data.width//2-20,data.height//6,data.width//2+20,
+    data.height//6+40],[data.width//2-20,data.height-data.height//5,
+    data.width//2+20,data.height-data.height//5+40]]
+    data.direction1 = 5
+    data.direction2 = -5
     data.startCircle = [data.width//2-30,data.height//2-30,data.width//2+30,
     data.height//2+30]
-    data.startCircleDirection = 1
-    data.timerDelay = 500
+    data.startGhost = [data.startCircle[0]-80,data.startCircle[1],
+    data.startCircle[2]-80,data.startCircle[3]]
+    data.startLost = False
+    data.timerDelay = 100
+    data.count = 0
     data.score = 0
     data.numRows = data.height//20
     data.numCols = data.width//20
@@ -47,31 +55,42 @@ def keyPressed(event, data):
             data.pacmanDirection = "Up"
         if event.keysym == "Down":
             data.pacmanDirection = "Down"
+    if data.loseState or data.winState:
+        if event.keysym == "p":
+            init()
     
 def timerFired(data):
-    if data.instructionState:
+    if data.startState:
+        moveStartCircles(data)
+    if data.instructionState and (data.startLost == False):
         #moves sample pac man left to right across the screen
-        data.startCircle[0] += data.startCircleDirection
-        data.startCircle[2] += data.startCircleDirection
-        if data.startCircle[0] <= data.margin or \
-        data.startCircle[2] >= data.width - data.margin:
-            data.startCircleDirection *= -1
+        data.startCircle[0] += 4
+        data.startCircle[2] += 4
+        data.startGhost[0] += 5
+        data.startGhost[2] += 5
+        if data.startCircle[0] <= data.startGhost[2]:
+            data.startLost = True
     if data.gameState:
         #print(validToMove(data))
         if validToMove(data):
-            movePacman(data)
-            if getCoins(data):
-                data.score += 1
+            if data.count%4 == 0:
+                movePacman(data)
+                if getCoins(data):
+                    data.score += 1
+        data.count += 1
 
 def redrawAll(canvas, data):
     canvas.create_rectangle(data.margin,data.margin,data.width-data.margin,
     data.height-data.margin,fill="black")
     if data.startState:
         #draws title and instructions to begin
-        canvas.create_text(data.width//2,data.height//2,
-        text="Welcome to Pac-Man",fill="white",font="Arial 50 bold")
+        canvas.create_text(data.width//2,data.height//3,
+        text="Welcome to Pac-Man",fill="plum",font="Arial 50 bold")
         canvas.create_text(data.width//2,data.height//2+60,
-        text="Press 'b' to begin",fill="white",font="Arial 32 bold")
+        text="Press 'b' to begin",fill="powderblue",font="Arial 32 bold")
+        for circle in data.startCircles:
+            canvas.create_oval(circle[0],circle[1],circle[2],circle[3],
+            fill="yellow")
     if data.instructionState:
         #draws instructions on how to place the game
         canvas.create_text(data.width//2,data.height//4,
@@ -83,6 +102,12 @@ def redrawAll(canvas, data):
         #draws example of pac man moving across the screen
         canvas.create_oval(data.startCircle[0],data.startCircle[1],
         data.startCircle[2],data.startCircle[3],fill="yellow")
+        #draws ghost following pacman
+        canvas.create_oval(data.startGhost[0],data.startGhost[1],
+        data.startGhost[2],data.startGhost[3],fill="red")
+        if data.startLost:
+            canvas.create_text(data.width//2,data.height//2+70,text="YOU LOST",
+            font="Arial 34 bold", fill="plum")
         #draws the button to continue to the game
         canvas.create_rectangle(220,450,380,500,fill="white")
         canvas.create_text(data.width//2,475,text="Continue",font="Arial 28 bold",
@@ -97,35 +122,35 @@ def redrawAll(canvas, data):
         10,350,10,290,110,290,110,200,40,200,
         50,50,550,50,550,190,480,190,480,300,590,300,590,340,480,340,
         480,410,550,410,550,580,50,580,50,410,120,410,120,340,10,340,10,300,
-        120,300,120,190,50,190,fill="blue")
-        canvas.create_rectangle(40,40,50,200,fill="blue",outline="blue")
+        120,300,120,190,50,190,fill="black",outline="blue")
+        canvas.create_rectangle(40,40,50,200,fill="black",outline="blue")
         #draws the coins
         drawCoins(data)
         for coin in data.coins:
             canvas.create_oval(coin[0],coin[1],coin[2],coin[3],fill="pink")
         #draws the inside barriers
         #top left barrier
-        canvas.create_rectangle(95,100,160,155,fill="blue")
+        canvas.create_rectangle(95,100,160,155,fill="black",outline="blue")
         #top middle barrier
         canvas.create_polygon(270,100,330,100,330,200,415,200,415,235,
-        185,235,185,200,270,200,fill="blue")
+        185,235,185,200,270,200,fill="black",outline="blue")
         #top right barrier
-        canvas.create_rectangle(505,100,440,155,fill="blue")
+        canvas.create_rectangle(505,100,440,155,fill="black",outline="blue")
         #middle left barrier
-        canvas.create_rectangle(170,365,280,440,fill="blue")
+        canvas.create_rectangle(170,365,280,440,fill="black",outline="blue")
         #middle right barrier
-        canvas.create_rectangle(430,365,320,440,fill="blue")
+        canvas.create_rectangle(430,365,320,440,fill="black",outline="blue")
         #bottom left barrier
         canvas.create_polygon(95,450,160,450,160,490,213,490,213,540,95,540,
-        fill="blue")
+        fill="black",outline="blue")
         #bottom middle barrier
-        canvas.create_rectangle(265,490,335,540,fill="blue")
+        canvas.create_rectangle(265,490,335,540,fill="black",outline="blue")
         #bottom right barrier
         canvas.create_polygon(505,450,440,450,440,490,387,490,387,540,505,540,
-        fill="blue")
+        fill="black",outline="blue")
         #draws where the ghosts stay
-        canvas.create_rectangle(240,280,360,330,fill="white")
-        canvas.create_rectangle(280,270,320,280,fill="white")
+        canvas.create_rectangle(240,280,360,330,fill="black",outline="blue")
+        canvas.create_rectangle(280,270,320,280,fill="black",outline="blue")
         #draws pac man
         canvas.create_oval(data.pacman[0],data.pacman[1],data.pacman[2],
         data.pacman[3],fill="yellow")
@@ -133,6 +158,36 @@ def redrawAll(canvas, data):
         for ghost in data.ghosts:
             canvas.create_oval(ghost[0],ghost[1],ghost[2],ghost[3],
             fill=ghost[4])
+    if data.loseState:
+        scoreText = "Score: %d" % (data.score)
+        canvas.create_text(data.width//2,data.height//3+35,text="YOU LOST",
+        font="Arial 40 bold",fill="plum")
+        canvas.create_text(data.width//2,data.height//3+85,
+        text=scoreText,font="Arial 30",fill="yellow")
+        canvas.create_text(data.width//2,data.height//3+135,
+        text="Press 'p' to play again",font="Arial 22",fill="plum")
+    if data.winState:
+        scoreText = "Score: %d" % (data.score)
+        canvas.create_text(data.width//2,data.height//3+35,text="YOU WON",
+        font="Arial 40 bold",fill="plum")
+        canvas.create_text(data.width//2,data.height//3+85,
+        text=scoreText,font="Arial 30",fill="yellow")
+        canvas.create_text(data.width//2,data.height//3+135,
+        text="Press 'p' to play again",font="Arial 22",fill="plum")
+            
+def moveStartCircles(data):
+    data.startCircles[0][0] += data.direction1
+    data.startCircles[0][2] += data.direction1
+    data.startCircles[1][0] += data.direction2
+    data.startCircles[1][2] += data.direction2
+    if data.startCircles[0][0] <= data.margin:
+        data.direction1 = 5
+    if data.startCircles[0][2] >= data.width-data.margin:
+        data.direction1 = -5
+    if data.startCircles[1][0] <= data.margin:
+        data.direction2 = 5
+    if data.startCircles[1][2] >= data.width-data.margin:
+        data.direction2 = -5
         
 def drawCoins(data):
     result = []
